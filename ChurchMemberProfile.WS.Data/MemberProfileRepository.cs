837363fbc4d89e8d631a1ecdbfba04627f6a9b7a
meanprogrammer <mean.programmer.d@gmail.com>
2015-08-14 12:18:26 +0800

    irepository and split entities

---
+++ ChurchMemberProfile.WS.Data/MemberProfileRepository.cs
@@ -0,0 +1,38 @@
+﻿using ChurchMemberProfile.WS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchMemberProfile.WS.Data
+{
+    public class MemberProfileRepository : IRepository<MemberProfile>
+    {
+
+        public MemberProfile GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MemberProfile> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOnSubmit(MemberProfile entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOnSubmit(MemberProfile entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubmitChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
