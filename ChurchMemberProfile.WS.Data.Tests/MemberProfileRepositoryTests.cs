b3018391118c37336256c11c55c109526e07a6ae
meanprogrammer <mean.programmer.d@gmail.com>
2015-08-20 16:47:08 +0800

    unit test

--- ChurchMemberProfile.WS.Data.Tests/MemberProfileRepositoryTests.cs
+++ ChurchMemberProfile.WS.Data.Tests/MemberProfileRepositoryTests.cs
@@ -5,17 +5,45 @@ using System.Text;
 using ChurchMemberProfile.WS.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ChurchMemberProfile.WS.Model;
+using ChurchMemberProfile.WS.Data.Core;
 namespace ChurchMemberProfile.WS.Data.Tests
 {
     [TestClass()]
     public class MemberProfileRepositoryTests
     {
         MemberProfileRepository repo = new MemberProfileRepository();
+
+        [ClassInitialize]
+        public void init()
+        {
+            MemberProfile m1 = new MemberProfile() { 
+                RecordID = 1,
+                Lastname = "Dudan",
+                Firstname = "Valiant",
+                MI = "A",
+                Nickname = "Dudz",
+                Address = "Caloocan City",
+                Birthdate = new DateTime(1986, 8, 14),
+                LeaderId = 11
+            };
+
+            repo.InsertOnSubmit(m1);
+        }
+
+        [ClassCleanup]
+        public void cleanup()
+        {
+            using (ChurchMemberProfileEntities context = new ChurchMemberProfileEntities())
+            {
+                context.Database.ExecuteSqlCommand("TRUNCATE TABLE MemberProfile");
+            }
+        }
+
         [TestMethod()]
         public void GetByIdTest()
         {
             MemberProfile result = repo.GetById(1);
-            Assert.IsNull(result);
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
