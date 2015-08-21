3f99fce555497da98fca7c3751b919e0b2a1d5c9
meanprogrammer <mean.programmer.d@gmail.com>
2015-08-21 16:11:20 +0800

    updates

---
+++ ChurchMemberProfile.WS.Data/MemberProfilePropertyRepository.cs
@@ -0,0 +1,31 @@
+﻿using ChurchMemberProfile.WS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChurchMemberProfile.WS.Data
+{
+    public class MemberProfilePropertyRepository : IRepository<MemberProfilePropertyValue>
+    {
+        public MemberProfilePropertyValue GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MemberProfilePropertyValue> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOnSubmit(MemberProfilePropertyValue entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOnSubmit(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChurchMemberProfile.WS.Data/PropertyDefinitionRepository.cs
@@ -0,0 +1,31 @@
+﻿using ChurchMemberProfile.WS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChurchMemberProfile.WS.Data
+{
+    public class PropertyDefinitionRepository : IRepository<MemberProfilePropertyDefinition>
+    {
+        public MemberProfilePropertyDefinition GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MemberProfilePropertyDefinition> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOnSubmit(MemberProfilePropertyDefinition entity)
+        {
+            
+        }
+
+        public void DeleteOnSubmit(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
