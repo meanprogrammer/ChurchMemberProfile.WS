468657aee6053ff41fa58bc3031942db9fff3853
meanprogrammer <mean.programmer.d@gmail.com>
2015-08-25 17:15:56 +0800

    unit test

--- ChurchMemberProfile.WS.Data/MemberProfilePropertyRepository.cs
+++ ChurchMemberProfile.WS.Data/MemberProfilePropertyRepository.cs
@@ -1,4 +1,5 @@
-﻿using ChurchMemberProfile.WS.Model;
+﻿using ChurchMemberProfile.WS.Data.Core;
+using ChurchMemberProfile.WS.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,22 @@ namespace ChurchMemberProfile.WS.Data
 {
     public class MemberProfilePropertyRepository : IRepository<MemberProfilePropertyValue>
     {
+        private int memberId;
+        public MemberProfilePropertyRepository(int memberId)
+        {
+            this.memberId = memberId;
+        }
+
+        private MemberProfilePropertyRepository() { }
+
         public MemberProfilePropertyValue GetById(int id)
         {
-            throw new NotImplementedException();
+            MemberProfilePropertyValue prop = null;
+            using (ChurchMemberProfileEntities context = new ChurchMemberProfileEntities())
+            {
+                //prop = context.MemberProfilePropertyValues.Where(c=>c.)
+            }
+            return null;
         }
 
         public IEnumerable<MemberProfilePropertyValue> GetAll()
--- ChurchMemberProfile.WS.Data/PropertyDefinitionRepository.cs
+++ ChurchMemberProfile.WS.Data/PropertyDefinitionRepository.cs
@@ -1,4 +1,5 @@
-﻿using ChurchMemberProfile.WS.Model;
+﻿using ChurchMemberProfile.WS.Data.Core;
+using ChurchMemberProfile.WS.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +9,47 @@ namespace ChurchMemberProfile.WS.Data
 {
     public class PropertyDefinitionRepository : IRepository<MemberProfilePropertyDefinition>
     {
+
         public MemberProfilePropertyDefinition GetById(int id)
         {
-            throw new NotImplementedException();
+            MemberProfilePropertyDefinition def = null;
+            using (ChurchMemberProfileEntities context = new ChurchMemberProfileEntities())
+            {
+                def = context.MemberProfilePropertyDefinitions.Where(c => c.RecordID == id).FirstOrDefault();
+            }
+            return def;
         }
 
         public IEnumerable<MemberProfilePropertyDefinition> GetAll()
         {
-            throw new NotImplementedException();
+            IEnumerable<MemberProfilePropertyDefinition> defs = null;
+            using (ChurchMemberProfileEntities context = new ChurchMemberProfileEntities())
+            {
+                defs = context.MemberProfilePropertyDefinitions.ToList();
+            }
+            return defs;
         }
 
         public void InsertOnSubmit(MemberProfilePropertyDefinition entity)
         {
-            
+            using (ChurchMemberProfileEntities context = new ChurchMemberProfileEntities())
+            {
+                context.MemberProfilePropertyDefinitions.Add(entity);
+                context.SaveChanges();
+            }
         }
 
         public void DeleteOnSubmit(int id)
         {
-            throw new NotImplementedException();
+            using (ChurchMemberProfileEntities context = new ChurchMemberProfileEntities())
+            {
+                MemberProfilePropertyDefinition def = context.MemberProfilePropertyDefinitions.Where(c => c.RecordID == id).FirstOrDefault();
+                if (def != null)
+                {
+                    context.MemberProfilePropertyDefinitions.Remove(def);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
