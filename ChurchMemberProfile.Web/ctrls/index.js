0cc9c80354dbcbdedfb491fe1c8095356736ec4a
meanprogrammer <mean.programmer.d@gmail.com>
2015-09-16 20:26:27 +0800

    added web to the project

---
+++ ChurchMemberProfile.Web/ctrls/index.js
@@ -0,0 +1,5 @@
+﻿angular.module('vpApp').controller('IndexCtrl', ['$scope', 
+    function ($scope) {
+        console.log('test');
+    }
+]);
---
+++ ChurchMemberProfile.Web/ctrls/template.js
@@ -0,0 +1,38 @@
+﻿angular.module('vpApp').controller('TemplateCtrl', ['$scope', '$modal',
+    function ($scope, $modal) {
+        
+        $scope.openModal = function () {
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'createTemplateModal.html',
+                controller: 'CreateTemplateModalInstanceCtrl',
+                size: 'md',
+                resolve: {
+                    
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+
+    }
+]);
+
+
+angular.module('vpApp').controller('CreateTemplateModalInstanceCtrl', function ($scope, $modalInstance) {
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss();
+    };
+
+    $scope.load = function () {
+        
+    };
+
+    $scope.load();
+});
